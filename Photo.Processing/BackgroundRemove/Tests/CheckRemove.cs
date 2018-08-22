c0c81a7af774ed3786771325600cce898d28032a
arkhivania <arkhivania@gmail.com>
2018-08-22 08:50:19 +0300

    samples

---
+++ Photo.Processing/BackgroundRemove/Tests/CheckRemove.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Photo.Processing.BackgroundRemove.Tests
+{
+    [TestFixture]
+    class CheckRemove
+    {
+        [Test]
+        public void TestRB()
+        {
+
+        }
+    }
+}
