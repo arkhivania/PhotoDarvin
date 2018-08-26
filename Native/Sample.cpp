2432e391827d7596f0ac1856ad379e090a6b6dd1
arkhivania <arkhivania@gmail.com>
2018-08-26 10:05:35 +0300

    opencv compilation settings

--- Native/Sample.cpp
+++ Native/Sample.cpp
@@ -1,5 +1,11 @@
 #include "stdafx.h"
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+
 extern "C" DLL_PUBLIC int Sample_Call(int a)
 {
 	return a * a;
